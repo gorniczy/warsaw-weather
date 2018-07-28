2e4a6637199fa42ebdac9d45822dbaf9cfdd13b9
gorniczy <denysgornichar@gmail.com>
2018-07-28 18:11:16 +0200

    apply style to return div

--- src/index.js
+++ src/index.js
@@ -4,5 +4,39 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class ShowWeather extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      main: "",
+      temperature: ""
+    };
+
+  }
+
+  componentDidMount() {
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=Warsaw,pl&appid=b5daf7d7450518f7ba259ab775096921&units=metric")
+      .then(res => res.json())
+      .then(
+        (result) => {
+                this.setState({
+                  main: result.weather[0].main,
+                  temperature: result.main.temp
+                });
+            },
+          )
+        }
+
+  render() {
+    return (
+      <div style={{position: "absolute", top: "30%", left: "20%"}}>
+        <p style={{fontFamily: "Arial", fontSize: 40, color: "#4182BD"}}>{this.state.main}</p>
+        <p style={{fontFamily: "Arial", fontSize: 40, color: "#BD4141"}}>{this.state.temperature}˚C</p>
+      </div>
+  )
+}
+
+}
+
+ReactDOM.render(<ShowWeather />, document.getElementById('root'));
 registerServiceWorker();
