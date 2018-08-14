391032c33bee0fae944f5588ff25cad9330374b5
gorniczy <denysgornichar@gmail.com>
2018-08-14 21:10:09 +0200

    add districts images

--- src/Skin.js
+++ src/Skin.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './Skin.css';
+import ochota_img from './img/Ochota.jpeg';
+import wola_img from './img/Wola.jpg';
 
 export const Skin = (props) => {
 
-    return <p className="title">{props.value}</p>
+    return (<div>
+              <p className="title">{props.value.toUpperCase()}</p>
+              {props.value === "Ochota" && <img className="facade" src={ochota_img} alt="picture of a house facade" />}
+              {props.value === "Wola" && <img className="facade" src={wola_img} alt="picture of a house facade" />}
+            </div>
+            );
 
 }
