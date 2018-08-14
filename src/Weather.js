90047d442f5c363de2aa0de5cc2f04c42106cff4
gorniczy <denysgornichar@gmail.com>
2018-08-14 21:10:42 +0200

    add className to Weather

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import './Weather.css';
 
 export const Weather = (props) => {
   return (
     <div>
-      <p style={{fontFamily: "Arial", fontSize: 40, color: "#4182BD"}}>Main: {props.main}</p>
-      <p style={{fontFamily: "AvenirNextCondensed-UltraLight", fontSize: 250, color: "#BD4141", margin: "0"}}>{props.temperature}˚C</p>
-      <p style={{fontFamily: "Arial", fontSize: 40, color: "#43A12C"}}>{props.wind} m/s</p>
+      <p className="tempC">{props.temperature}˚C</p>
     </div>
   );
 }
