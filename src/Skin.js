badc710e784a0324d70fce63a20d242afe3c688c
gorniczy <denysgornichar@gmail.com>
2018-08-19 19:27:20 +0200

    set value to upper case words

--- src/Skin.js
+++ src/Skin.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Skin.css';
 import ochota_img from './img/Ochota.jpeg';
-import wola_img from './img/Wola.jpg';
+import wola_img from './img/Wola.JPG';
 
 export const Skin = (props) => {
 
     return (<div>
-              <p className="title">{props.value.toUpperCase()}</p>
-              {props.value === "Ochota" && <img className="facade" src={ochota_img} alt="picture of a house facade" />}
-              {props.value === "Wola" && <img className="facade" src={wola_img} alt="picture of a house facade" />}
+              <p className="title">{props.value}</p>
+              {props.value === "OCHOTA" && <img className="facade" src={ochota_img} alt="house facade" />}
+              {props.value === "WOLA" && <img className="facade" src={wola_img} alt="house facade" />}
             </div>
             );
 
