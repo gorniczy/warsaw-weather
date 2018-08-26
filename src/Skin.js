148f20a1ee5aaab0aa8bcb3000fea49bafc5e218
gorniczy <denysgornichar@gmail.com>
2018-08-26 23:04:22 +0200

    add the third image for skin
    (as a placeholder)

--- src/Skin.js
+++ src/Skin.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Skin.css';
 import ochota_img from './img/Ochota.jpeg';
 import wola_img from './img/Wola.JPG';
+import mokotow_img from './img/Mokotów.JPG';
 
 export const Skin = (props) => {
 
@@ -9,6 +10,7 @@ export const Skin = (props) => {
               <p className="title">{props.value}</p>
               {props.value === "OCHOTA" && <img className="facade" src={ochota_img} alt="house facade" />}
               {props.value === "WOLA" && <img className="facade" src={wola_img} alt="house facade" />}
+              {props.value === "MOKOTÓW" && <img className="facade" src={mokotow_img} alt="house facade" />}
             </div>
             );
 
