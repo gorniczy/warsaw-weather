e0739d6972454a7956c5e3c01d2b4587da595a6a
gorniczy <denysgornichar@gmail.com>
2019-03-15 13:00:08 +0100

    Updates

---
+++ precache-manifest.f1c107b1335b1e453b5c14dd7e37d85f.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "c273e09e7cb242108d4f92c3d010a0e9",
+    "url": "/warsaw-weather/static/media/Rain Sun.c273e09e.png"
+  },
+  {
+    "revision": "b722d5bd6116c9c42932",
+    "url": "/warsaw-weather/static/css/main.53c19e7e.chunk.css"
+  },
+  {
+    "revision": "9738fab3c6da9254729c",
+    "url": "/warsaw-weather/static/js/runtime~main.80d1640b.js"
+  },
+  {
+    "revision": "d734e8c77c9222c83e92",
+    "url": "/warsaw-weather/static/js/2.679b6b47.chunk.js"
+  },
+  {
+    "revision": "d6f03d98dc73c9c64f415f2a0279b824",
+    "url": "/warsaw-weather/static/media/Ochota.d6f03d98.jpeg"
+  },
+  {
+    "revision": "538dc80ca8170648fcc81cd5b2641881",
+    "url": "/warsaw-weather/static/media/Wola.538dc80c.JPG"
+  },
+  {
+    "revision": "5d8e8626fcd0990c31ce2836b04f30ff",
+    "url": "/warsaw-weather/static/media/Mokotów.5d8e8626.JPG"
+  },
+  {
+    "revision": "6918790c92c672ec4d3ab08f450aa2f0",
+    "url": "/warsaw-weather/static/media/Clear Sky Sun.6918790c.png"
+  },
+  {
+    "revision": "4ead48f610355b55e8fb86fc24e746dc",
+    "url": "/warsaw-weather/static/media/Clear Sky Moon.4ead48f6.png"
+  },
+  {
+    "revision": "eb4e5e9fbdd4a263e8e059e1c48187c9",
+    "url": "/warsaw-weather/static/media/Few Clouds Sun.eb4e5e9f.png"
+  },
+  {
+    "revision": "b382f218a5bd2d2717e2f641c137fcd3",
+    "url": "/warsaw-weather/static/media/Few Clouds Moon.b382f218.png"
+  },
+  {
+    "revision": "813a8c6fd3ba537137b31a768d612f29",
+    "url": "/warsaw-weather/static/media/Scattered Clouds.813a8c6f.png"
+  },
+  {
+    "revision": "b63296ad2365f3ec87a8599d6c43d1d0",
+    "url": "/warsaw-weather/static/media/Broken Clouds.b63296ad.png"
+  },
+  {
+    "revision": "b722d5bd6116c9c42932",
+    "url": "/warsaw-weather/static/js/main.a0c247dc.chunk.js"
+  },
+  {
+    "revision": "3f6e10315f92b28c0226c3a8f457c222",
+    "url": "/warsaw-weather/static/media/Rain Moon.3f6e1031.png"
+  },
+  {
+    "revision": "7159db5d3ba79a423bdb41c069d24ed8",
+    "url": "/warsaw-weather/static/media/Shower Rain.7159db5d.png"
+  },
+  {
+    "revision": "20e06200da6a88f4f0108ab1173adeaa",
+    "url": "/warsaw-weather/static/media/Thunderstorm.20e06200.png"
+  },
+  {
+    "revision": "9dfa3e584a216042425690d8cb0b041b",
+    "url": "/warsaw-weather/static/media/Snow.9dfa3e58.png"
+  },
+  {
+    "revision": "b9b72a1fa6ac41bcb9d447c8fccd2f73",
+    "url": "/warsaw-weather/static/media/Mist Sun.b9b72a1f.png"
+  },
+  {
+    "revision": "3ebe21d9d39f34d901653e01e8f05dca",
+    "url": "/warsaw-weather/static/media/Mist Moon.3ebe21d9.png"
+  },
+  {
+    "revision": "52437aa0b1f7a8f1353804e92eede01a",
+    "url": "/warsaw-weather/static/media/Day-background-cover.52437aa0.png"
+  },
+  {
+    "revision": "44c5f99826783cbc0e6d2489923f2f20",
+    "url": "/warsaw-weather/static/media/Night-background-cover.44c5f998.png"
+  },
+  {
+    "revision": "d5c4fcb7b8d652dd7d6b7786f027a98d",
+    "url": "/warsaw-weather/static/media/Day-background.d5c4fcb7.png"
+  },
+  {
+    "revision": "14887f0b2b933ceb9fe7eb1ec5ae95f3",
+    "url": "/warsaw-weather/static/media/Night-background.14887f0b.png"
+  },
+  {
+    "revision": "3f326f52e09cd51b035f0f082596ef56",
+    "url": "/warsaw-weather/static/media/AvenirNextCondensed-Regular.3f326f52.ttf"
+  },
+  {
+    "revision": "0f21db472aff41076cc06b9421dc9a4d",
+    "url": "/warsaw-weather/static/media/AvenirNextCondensed-UltraLight.0f21db47.ttf"
+  },
+  {
+    "revision": "9b1c27d374d2f0e421eb1eb5f0b9db9f",
+    "url": "/warsaw-weather/index.html"
+  }
+];
