f3805aaff5affbf7dd88c5ed5075632d73b02625
gorniczy <denysgornichar@gmail.com>
2019-05-29 23:06:55 +0200

    use state destructuring in DateTime component

--- src/components/DateTime.js
+++ src/components/DateTime.js
@@ -1,44 +1,32 @@
-import React from "react";
-import "./DateTime.css";
+import React from "react"
+import "./DateTime.css"
 
 export class DateTime extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       year: "",
       month: "",
       date: "",
       day: "",
       time: ""
-    };
+    }
   }
 
   componentDidMount() {
-    this.loadTime();
-    this.timer = setInterval(() => this.loadTime(), 1000);
+    this.loadTime()
+    this.timer = setInterval(() => this.loadTime(), 1000)
   }
 
   componentWillUnmount() {
-    clearInterval(this.timer);
+    clearInterval(this.timer)
   }
 
   loadTime = () => {
-    const date = new Date();
-    const day = ["ND", "PN", "WT", "ŚR", "CZW", "PT", "SB"];
-    const month = [
-      "I",
-      "II",
-      "III",
-      "IV",
-      "V",
-      "VI",
-      "VII",
-      "VIII",
-      "IX",
-      "X",
-      "XI",
-      "XII"
-    ];
+    const date = new Date()
+    const day = ["ND", "PN", "WT", "ŚR", "CZW", "PT", "SB"]
+    const month =
+    ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII"]
     this.setState({
       year: date.getFullYear(),
       month: month[date.getMonth()],
@@ -48,15 +36,16 @@ export class DateTime extends React.Component {
         ("0" + date.getHours()).slice(-2) +
         ":" +
         ("0" + date.getMinutes()).slice(-2)
-    });
-  };
+    })
+  }
 
   render() {
+    const { fontSize } = this.props
+    const { year, day, date, month, time } = this.state
     return (
-      <p className="date_time" style={{ fontSize: this.props.fontSize }}>
-        {this.state.day}
-        {this.state.date}.{this.state.month}.{this.state.year} {this.state.time}
+      <p className="date_time" style={{ fontSize: fontSize }}>
+        {day} {date}.{month}.{year} {time}
       </p>
-    );
+    )
   }
 }
