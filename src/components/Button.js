344e882d4f591e2c0dc64e2060fad2de09bfc680
gorniczy <denysgornichar@gmail.com>
2019-06-01 19:14:39 +0200

    place button and dropdown into a nav-responsive div

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,30 +4,32 @@ import "./Button.css"
 
 export const Button = props => {
   return (
-    <div className="nav_box" onMouseEnter={props.dropdown} onMouseLeave={props.dropdown}>
-      <div className="btn">
-        <img src={btn_img} className="btn_img" alt="button" />
+    <div className="nav_box">
+      <div className="nav-responsive" onMouseEnter={props.dropdown} onMouseLeave={props.dropdown}>
+        <div className="btn">
+          <img src={btn_img} className="btn_img" alt="button" />
+        </div>
+        <ul className="dropdown" style={{ display: props.showDistricts }}>
+          <li
+            onClick={() => props.skin("Ochota")}
+            style={{ fontSize: props.fontSizeNav }}
+          >
+            Ochota
+          </li>
+          <li
+            onClick={() => props.skin("Wola")}
+            style={{ fontSize: props.fontSizeNav }}
+          >
+            Wola
+          </li>
+          <li
+            onClick={() => props.skin("Mokotów")}
+            style={{ fontSize: props.fontSizeNav }}
+          >
+            Mokotów
+          </li>
+        </ul>
       </div>
-      <ul className="dropdown" style={{ display: props.showDistricts }}>
-        <li
-          onClick={() => props.skin("Ochota")}
-          style={{ fontSize: props.fontSizeNav }}
-        >
-          Ochota
-        </li>
-        <li
-          onClick={() => props.skin("Wola")}
-          style={{ fontSize: props.fontSizeNav }}
-        >
-          Wola
-        </li>
-        <li
-          onClick={() => props.skin("Mokotów")}
-          style={{ fontSize: props.fontSizeNav }}
-        >
-          Mokotów
-        </li>
-      </ul>
       <p className="title" style={{ fontSize: props.fontSizeTitle }}>
         {props.value}
       </p>
