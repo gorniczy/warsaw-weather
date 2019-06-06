0f661bc1f039316b4b9de4892a33ab7628c9a60f
gorniczy <denysgornichar@gmail.com>
2019-06-06 22:01:40 +0200

    add condition to onMouseLeave event and destructure props of Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,39 +3,37 @@ import btn_img from "../img/button.png"
 import "./Button.css"
 
 export const Button = props => {
+  const {
+    showDistricts,
+    dropdown,
+    fontSizeTitle,
+    value,
+    skin,
+    fontSizeNav
+  } = props
   return (
-    <div className="navbar" onMouseLeave={props.dropdown}>
+    <div
+      className="navbar"
+      onMouseLeave={() => (showDistricts === "flex" ? dropdown() : null)}
+    >
       <div className="nav_box">
-        <div
-          className="nav-responsive"
-          onClick={props.dropdown}
-
-        >
+        <div className="nav-responsive" onClick={() => dropdown()}>
           <div className="btn">
             <img src={btn_img} className="btn_img" alt="button" />
           </div>
         </div>
-        <p className="title" style={{ fontSize: props.fontSizeTitle }}>
-          {props.value}
+        <p className="title" style={{ fontSize: fontSizeTitle }}>
+          {value}
         </p>
       </div>
-      <ul className="dropdown" style={{ display: props.showDistricts }}>
-        <li
-          onClick={() => props.skin("Ochota")}
-          style={{ fontSize: props.fontSizeNav }}
-        >
+      <ul className="dropdown" style={{ display: showDistricts }}>
+        <li onClick={() => skin("Ochota")} style={{ fontSize: fontSizeNav }}>
           Ochota
         </li>
-        <li
-          onClick={() => props.skin("Wola")}
-          style={{ fontSize: props.fontSizeNav }}
-        >
+        <li onClick={() => skin("Wola")} style={{ fontSize: fontSizeNav }}>
           Wola
         </li>
-        <li
-          onClick={() => props.skin("Mokotów")}
-          style={{ fontSize: props.fontSizeNav }}
-        >
+        <li onClick={() => skin("Mokotów")} style={{ fontSize: fontSizeNav }}>
           Mokotów
         </li>
       </ul>
